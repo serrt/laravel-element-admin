52863edff439c51cc595fb0ef5c50f79c2d372f8
panliang <1163816051@qq.com>
2021-08-09 11:09:30 +0800

    keep-alive

--- src/views/empty.vue
+++ src/views/empty.vue
@@ -1,7 +1,15 @@
 <template>
   <transition name="fade-transform" mode="out-in">
-    <router-view />
+    <router-view :key="$route.fullPath" />
   </transition>
+
+  <!-- 使用 meta.cache 缓存页面; 组件刷新事件 activated -->
+  <!-- <div>
+    <keep-alive>
+      <router-view v-if="$route.meta && $route.meta.cache" :key="$route.fullPath" />
+    </keep-alive>
+    <router-view v-if="!($route.meta && $route.meta.cache)" :key="$route.fullPath" />
+  </div> -->
 </template>
 
 <script>
