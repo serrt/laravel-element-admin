d804e497ba161cc3420a5239145fd51d9c8e2d0a
panliang <1163816051@qq.com>
2021-08-09 14:45:37 +0800

    token cache ke

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,18 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
-const guardKey = 'vue_admin_guard_name'
+const tokenKey = 'vue_admin_token'
+const guardKey = 'vue_admin_guard'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return Cookies.get(tokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(tokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return Cookies.remove(tokenKey)
 }
 
 export function guard(name) {
